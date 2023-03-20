c09701d842eb6bb5450c8df0924c78aae6fde20c
superernie77 <superernie77@gmail.com>
2023-03-20 10:25:38 +0100

    bean definitions and test

--- src/main/java/com/jot/Spring6DeclarativeHhtpInterfacesApplication.java
+++ src/main/java/com/jot/Spring6DeclarativeHhtpInterfacesApplication.java
@@ -1,13 +1,36 @@
 package com.jot;
 
+import com.jot.httpclients.PostsClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.support.WebClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 @SpringBootApplication
 public class Spring6DeclarativeHhtpInterfacesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Spring6DeclarativeHhtpInterfacesApplication.class, args);
+	}
+
+	private String serviceUrl = "https://jsonplaceholder.typicode.com/";
+
+	@Bean
+	public WebClient createWebClient(){
+		WebClient webClient = WebClient.builder()
+				.baseUrl(serviceUrl)
+				.build();
+		return webClient;
+	}
+
+	@Bean
+	public PostsClient createPostsClient(WebClient webClient){
+		HttpServiceProxyFactory httpServiceProxyFactory = HttpServiceProxyFactory
+				.builder(WebClientAdapter.forClient(webClient))
+				.build();
+		return httpServiceProxyFactory.createClient(PostsClient.class);
 	}
 
 }
--- src/test/java/com/jot/PostsClientTest.java
+++ src/test/java/com/jot/PostsClientTest.java
@@ -1,13 +1,29 @@
 package com.jot;
 
+import com.jot.domain.Post;
+import com.jot.httpclients.PostsClient;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 @SpringBootTest
-class Spring6DeclarativeHhtpInterfacesApplicationTests {
+class PostsClientTest {
+
+	@Autowired
+	private PostsClient postsClient;
 
 	@Test
-	void contextLoads() {
+	public void testPostsClientGet() {
+
+		List<Post> posts = postsClient.getAllPosts();
+
+
+
+		assertNotNull(posts);
 	}
 
 }
